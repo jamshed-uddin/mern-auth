449c80b0afb294d95a1e2af5fd1970168757d954
Jami <jamsheduddin03@gmail.com>
2024-01-28 21:39:12 +0600

    user creation and hashing password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -11,7 +11,26 @@ const authUser = asyncHandler(async (req, res) => {
 //access public
 const registerUser = asyncHandler(async (req, res) => {
   console.log(req.body);
-  res.status(200).json({ message: "Create new user" });
+
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User with this email already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user info");
+  }
 });
 //desc  create new user
 //route POST/api/users/logout
