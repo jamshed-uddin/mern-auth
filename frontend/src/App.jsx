32a0b587236512e1368b429cb91725ced884e941
Jami <jamsheduddin03@gmail.com>
2024-01-29 17:36:40 +0600

    frontend setup with tailwind react router daisyui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-xl">hello</h1>
+      <button className="btn btn-info">Info</button>
+    </div>
+  );
+}
+
+export default App;
