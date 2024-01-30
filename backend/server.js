b1a2056bcfd39f2ab676c5e572eadb7a79a783b7
Jami <jamsheduddin03@gmail.com>
2024-01-30 12:10:27 +0600

    removed cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
+
 dotenv.config();
 const port = process.env.PORT || 7000;
 import userRoutes from "./routes/userRoutes.js";
@@ -13,12 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
 
 app.use("/api/users", userRoutes);
 
